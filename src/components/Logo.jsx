28951ae4c2131bc2d634918a0e654f63318b329d
Preetam <preetam@appzoy.com>
2025-03-23 16:14:26 +0530

    align logo

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.png'
 
 const Logo = ({ height = "50px"}) => {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center" style={{paddingTop: '10px'}}>
       <img src={logo} alt="Company Logo" style={{ height }} />
     </div>
   );
